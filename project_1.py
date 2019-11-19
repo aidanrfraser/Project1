193983976982bf15eb694abf74052bb1ae9db781
Aidan Fraser <54653789+aidanrfraser@users.noreply.github.com>
2019-11-19 14:09:18 -0500

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -166,8 +166,8 @@ def get_score(location, grid, shape):
     score = 0
     for row in range(len(shape.squares)):
         for col in range(len(shape.squares[0])):
-            if shape.squares[row][col] == False: 
-                if grid.squares[location[0] + row][location[1] + col] == True:
+            if shape.squares[row][col] == False:
+                if grid.squares[location[0] + row - 1][location[1] + col - 1] == True:
                     score += 1
     return score
     
